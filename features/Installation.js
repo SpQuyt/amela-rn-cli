3e076126338dd8efbe430d80454f7ac2c35ad70b
SpQuyt <spquyt@gmail.com>
2021-08-02 17:05:48 +0700

    fix flipper post installer

--- features/Installation.js
+++ features/Installation.js
@@ -79,6 +79,11 @@ const editFiles = async (appName, appDisplayName) => {
             "use_flipper!()",
             "use_flipper!({ 'Flipper-Folly' => '2.5.3', 'Flipper' => '0.87.0', 'Flipper-RSocket' => '1.3.1' })"
         );
+        await CustomPromise.replaceStringFilePromise(
+            `${newPath}/ios/Podfile`,
+            "react_native_post_install(installer)",
+            "flipper_post_install(installer)"
+        );
         await CustomPromise.execCommandLinePromise(
             `cd ./${appName} && cd ios && pod install`,
             `Update flipper iOS to ${newPath}...`
