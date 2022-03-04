ed2b381225be630a87177f6438064fc8f15e65a8
Trương Anh Quốc <spquyt@gmail.com>
2022-03-04 15:06:52 +0700

    fix delete unused pulling from master

--- components/PostInstallation.js
+++ components/PostInstallation.js
@@ -1,14 +1,12 @@
 const ChangeAppIcon = require('../features/ChangeAppIcon');
 const CustomPromise = require('../promises');
 const Texts = require('../texts');
-const Git = require('./Git');
 const Simulator = require('./Simulator');
 
 const isWinOS = process.platform === 'win32';
 
+// eslint-disable-next-line no-unused-vars
 const exec = async (appName, repoURL) => {
-  // Delete README to avoid conflict and then pull master
-  Git.pullMaster({ appName, repoURL });
   // Change app icon to default AMELA icon
   const appNameWithoutHyphen = `${appName.trim().replace(/-/g, '').replace(/ /g, '')}`;
   await ChangeAppIcon.exec({ appNameWithoutHyphen, appName });
