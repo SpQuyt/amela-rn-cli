f54fc073e233d5cd6549960617d54821d915f775
QuocTA-Amela <101373375+QuocTA-Amela@users.noreply.github.com>
2022-05-21 21:56:29 +0700

    fix flipper podfile

--- components/config/Flipper.js
+++ components/config/Flipper.js
@@ -4,11 +4,6 @@ const config = async ({ appName }) => {
   const newPath = `./${appName}`;
   await CustomPromise.replaceStringFilePromise(
     `${newPath}/ios/Podfile`,
-    'use_flipper!()',
-    "use_flipper!({ 'Flipper-Folly' => '2.5.3', 'Flipper' => '0.87.0', 'Flipper-RSocket' => '1.3.1' })",
-  );
-  await CustomPromise.replaceStringFilePromise(
-    `${newPath}/ios/Podfile`,
     'react_native_post_install(installer)',
     'flipper_post_install(installer)',
   );
